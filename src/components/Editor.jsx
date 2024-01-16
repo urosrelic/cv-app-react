60bd5496a2b05786f4c0cd9551517cb776e93016
Uros Relic <urosrelicc@gmail.com>
2024-01-16 17:30:40 +0100

    refactor: remove contact component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,9 @@
 import { General } from "./General";
-import { Contact } from "./Contact";
 
 export const Editor = ({ setGeneralData }) => {
   return (
     <div className='editor-container'>
       <General setGeneralData={setGeneralData} />
-      <Contact />
     </div>
   );
 };
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -33,6 +33,22 @@ export const General = ({ setGeneralData }) => {
         />
       </div>
       <div className='input-container'>
+        <label>Email:</label>
+        <input type='email' id='email-input' />
+      </div>
+      <div className='input-container'>
+        <label>Location:</label>
+        <input type='text' id='location-input' />
+      </div>
+      <div className='input-container'>
+        <label>Website:</label>
+        <input type='text' id='website-input' />
+      </div>
+      <div className='input-container'>
+        <label>Phone:</label>
+        <input type='text' id='phone-input' />
+      </div>
+      <div className='input-container'>
         <label>Description:</label>
         <textarea
           id='description'
