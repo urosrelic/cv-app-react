54073e2e051bf93a393520caaf93ac3b0d5e8f2c
urosrelic <urosrelicc@gmail.com>
2024-01-18 13:41:46 +0100

    chore: minor styling changes

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -4,6 +4,7 @@ import { GeneralPreview } from "./preview/GeneralPreview";
 export const Preview = ({ generalData, educationData }) => {
   return (
     <div className='preview-container'>
+      <h2>Preview</h2>
       <div className='cv-container'>
         <GeneralPreview data={generalData} />
         <EducationPreview educationData={educationData} />
