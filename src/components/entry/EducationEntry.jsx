d4f16ffcb89ae7e734cc3dd4e0ecf88809b4f8d3
urosrelic <urosrelicc@gmail.com>
2024-01-18 14:50:56 +0100

    fix: dash being displayed on null dates

--- src/components/entry/EducationEntry.jsx
+++ src/components/entry/EducationEntry.jsx
@@ -4,11 +4,13 @@ export const EducationEntry = ({ entry }) => {
       <div className='education-preview-item-info'>
         <h3>{entry.degree}</h3>
         <p>{entry.school}</p>
-        {entry.student === true ? (
-          <p>{entry.startDate + " - Present"}</p>
-        ) : (
-          <p>{entry.startDate + " - " + entry.startDate}</p>
-        )}
+        {entry.startDate ? (
+          entry.student === true ? (
+            <p>{entry.startDate + " - Present"}</p>
+          ) : (
+            <p>{entry.startDate + " - " + entry.endDate}</p>
+          )
+        ) : null}
       </div>
     </div>
   );
