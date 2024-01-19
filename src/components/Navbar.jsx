75a5807ad2286dc41fc5cedc7bafe1e3f191d5d3
urosrelic <urosrelicc@gmail.com>
2024-01-19 15:03:43 +0100

    feat: add print functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 export const Navbar = () => {
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
     <div className='navbar'>
       <nav>
         <h1>CV Generator</h1>
         <div className='buttons'>
-          <div id='print-btn'>
+          <div id='print-btn' onClick={handlePrint}>
             <i className='fa-solid fa-print'></i>
           </div>
           <div id='load-example-btn'>
